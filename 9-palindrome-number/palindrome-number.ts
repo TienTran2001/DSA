a6808fb2689918c3a3f6f662bc1865c0c7f25cea
Trần Văn Tiến <65322557+TienTran2001@users.noreply.github.com>
2025-12-27 11:06:40 +0700

    Time: 5 ms (80.75%) | Memory: 64.5 MB (86.03%) - LeetSync

--- 9-palindrome-number/palindrome-number.ts
+++ 9-palindrome-number/palindrome-number.ts
@@ -1,6 +1,19 @@
+/**
+    time complexity: O(n)
+    sapce comlextity: O(1)
+ */
+
 function isPalindrome(x: number): boolean {
-    const str = x.toString()
-    const reverseStr = str.split("").reverse().join("")
-    if (str === reverseStr) return true
-    return false
+   if (x < 0) return false
+   if(x % 10 === 0 && x !== 0) return false
+
+   let reverse = 0
+   while (x > reverse) {
+        reverse = reverse * 10 + (x % 10)
+
+        x = Math.floor(x / 10)
+   }
+
+   return x === reverse || x === Math.floor(reverse / 10)
+
 };
