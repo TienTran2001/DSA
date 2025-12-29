64f44e180735a44ca22c3e9cc0b3265c9d6ed042
Trần Văn Tiến <65322557+TienTran2001@users.noreply.github.com>
2025-12-29 14:32:10 +0700

    Time: 1 ms (63.96%) | Memory: 56.9 MB (28.09%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,21 @@
+
+/**
+    lấy phần tử đầu tiên làm gốc để so sánh
+    time complexity: O(m * n)
+    space complexity: O(1)
+ */
+
+function longestCommonPrefix(strs: string[]): string {
+    if (strs.length === 0) return ""
+
+    for (let i = 0; i < strs[0].length; i++) {
+        const char = strs[0][i]
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i > strs[j].length || char !== strs[j][i]) return strs[0].slice(0, i) 
+        }
+
+    }
+
+    return strs[0]
+};
