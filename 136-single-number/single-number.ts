0441f37b3ecfd4cbfd836f9f540a7cd4870252cf
Trần Văn Tiến <65322557+TienTran2001@users.noreply.github.com>
2026-01-05 16:37:57 +0700

    Time: 3 ms (51.30%) | Memory: 58.6 MB (51.38%) - LeetSync

--- 136-single-number/single-number.ts
+++ 136-single-number/single-number.ts
@@ -1,14 +1,8 @@
 function singleNumber(nums: number[]): number {
-   const n = nums.length
-
-    if (n === 1) return nums[0]
-
-    let i = 0
     let res = 0
-    while (i < n) {
-        res ^= nums[i]
-       
-        i++
+
+    for (const num of nums) {
+        res ^= num
     }
 
     return res
